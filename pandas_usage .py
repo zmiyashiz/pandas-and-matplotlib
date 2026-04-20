0486354837ab785c5a70d1d00493c99bb006727b
tmr <bazarsadaev06@mail.ru>
2026-04-20 11:15:03 +0800

    Skibob

---
+++ pandas_usage .py
@@ -0,0 +1,77 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+df = pd.read_csv("exam_scores.csv")
+
+
+
+#BAR CHART
+
+plt.figure(figsize=(10, 5))
+plt.bar(df["Student"], df["Midterm Exam"], color="red")
+plt.xlabel("Student Names")
+plt.ylabel("Student Scores")
+plt.title("Student Exam Score")
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 1. show the dataframe entirely
+# print(df)
+
+# 2. See the top 5
+# print(df.head())
+
+# 3. Basic stats- min/max/avg
+# print(df.describe())
+
+# #4. Grab names only
+# print(df["Student"])
+
+# 5. Grab only scores
+
+# print(df["Final Exam"])
+
+# print(df["Final Exam"].mean())
+# print(df["Final Exam"].min())
+# print(df["Final Exam"].max())
+
+
+# 6. Grab Students Who got above 80
+
+print(df[df["Final Exam"]>80])
+
+# print(df[df["Final Exam"]==100])
